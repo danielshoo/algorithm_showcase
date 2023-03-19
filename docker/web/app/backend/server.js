cc0c76222764a248ce11c38429db39933d62ce51
Daniel Shuster <danielshuster@outlook.com>
2023-03-18 23:57:17 -0400

    [main] Added initial NodeJS server application that will listen locally on port 3000. Expected use is to recieve requests proxied by Nginx

---
+++ docker/web/app/backend/server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const querystring = require('node:querystring');
+
+const server = http.createServer((req, res) => {
+    
+    const urlPath = req.url;
+
+    let queryString = urlPath.split("?")?.[1] ?? '';
+    res.end("Successfully started a server: " + JSON.stringify({reqUrl: req.url, reqMethod: req.method, queryString: querystring.parse(queryString)}));
+});
+
+server.listen(3000, "localhost");
