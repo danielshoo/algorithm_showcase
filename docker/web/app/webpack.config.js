c5c11fd643b3efb1aa221e441749b771d85ca2a3
Daniel Shuster <danielshuster@outlook.com>
2023-03-19 18:23:34 -0400

    [main] webpack config updated to bundle js & (s)css dependencies for static html files

--- docker/web/app/webpack.config.js
+++ docker/web/app/webpack.config.js
@@ -1,10 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ChunkListWebpackPlugin = require('chunk-list-webpack-plugin');
+const webpack = require('webpack');
 const Path = require('path');
 const Fs = require('fs');
-
-const cssEntries = {};
-const jsEntries = {};
-const htmlEntries = {};
 
 function recursiveReadSync(startingDirectory, files = {}) {
     
@@ -17,32 +16,50 @@ function recursiveReadSync(startingDirectory, files = {}) {
         if (Fs.lstatSync(Path.join(__dirname, 'frontend', startingDirectory, file)).isDirectory()) {
             files = Object.assign(files, recursiveReadSync(Path.join(startingDirectory, file), files));
         } else {
-            files[startingDirectory + Path.sep + file] = Path.join(__dirname, 'frontend', startingDirectory, file);
+            files[startingDirectory + Path.sep + file.replace(/\..*$/, '')] = Path.join(__dirname, 'frontend', startingDirectory, file);
         }
     });
     
     return files;
 }
 
-const jsJiles = recursiveReadSync('js');
+const jsFiles = recursiveReadSync('js');
 const cssFiles = recursiveReadSync('css');
 const htmlFiles = recursiveReadSync('html');
 
-const entries = Object.assign(jsJiles, cssFiles);
-
+const entries = Object.assign(jsFiles, cssFiles);
+console.log(entries);
 htmlWebpackPlugins = Object.keys(htmlFiles).map((key) => {
     return new HtmlWebpackPlugin({
-        filename: key,
+        filename: key + '.html',
         template: htmlFiles[key],
     });
 });
 
 module.exports = {
-    entry: entries,
+    entry: jsFiles,
     target: 'web',
     output: {
         path: Path.resolve(__dirname, './assets'),
         filename: '[name].[contenthash].js',
     },
-    plugins: [...htmlWebpackPlugins],
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            emit: true, esModule: false
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [...htmlWebpackPlugins, new MiniCssExtractPlugin({})],
 };
+
